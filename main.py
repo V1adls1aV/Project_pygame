90a361cea2a7e04ea19703dd5eb119b2583db21c
Владислав <2006vladik@mail.ru>
2021-12-30 15:05:29 +0300

    version with car driving

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import sys
+from math import sin, cos, pi
 
 pygame.init()
 size = WIDTH, HEIGHT = 1920, 1020
@@ -9,6 +10,7 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 FPS = 60
 
+all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 
 
@@ -52,10 +54,12 @@ def load_level(name):
 
 def generate_level(level):
     new_level = [el[:] for el in level]
+    f = True
+    gamer = None
     for y in range(len(level)):
         for x in range(len(level[0])):
             if level[y][x] == '#':
-                new_level[y][x] = House(x, y)
+                new_level[y][x] = Empty(x, y)
             elif level[y][x] == '.':
                 if level[y + 1][x] == '.' and level[y - 1][x] == '.'\
                         and level[y][x + 1] == '.' and level[y][x - 1] == '.':
@@ -80,12 +84,15 @@ def generate_level(level):
                     new_level[y][x] = Road(x, y, 'I')
                 elif level[y][x + 1] == '.' or level[y][x - 1] == '.':
                     new_level[y][x] = Road(x, y, 'I', 90)
-    return new_level
+                if f:
+                    gamer = Player(x, y)
+                    f = False
+    return new_level, gamer
 
 
 class Road(pygame.sprite.Sprite):
     def __init__(self, x, y, mode, rotation=0):
-        super().__init__(tiles_group)
+        super().__init__(tiles_group, all_sprites)
         self.mode = mode
         self.rotation = rotation
         self.image = self.generate_road_image()
@@ -103,20 +110,134 @@ class Road(pygame.sprite.Sprite):
             return pygame.transform.rotate(load_image('line.png'), self.rotation)
 
 
-class House(pygame.sprite.Sprite):
+class Empty(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(tiles_group)
+        super().__init__(tiles_group, all_sprites)
         self.image = load_image('house.png')
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x * tile_width, y * tile_height
 
 
-city = generate_level(load_level('map.txt'))
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
+        self.image = load_image('car.png')
+        self.start_image = self.image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 25 + x * tile_width, 25 + y * tile_height
+        self.max_front_speed = 30
+        self.max_rear_speed = 7
+        self.speed = 0
+        self.rotation = 0
+        self.dx = 0
+        self.dy = 0
+
+    def update_speed(self, m):
+        if m == '+' and self.speed < self.max_front_speed:
+            self.speed += 0.5
+        if m == '-' and self.speed > -self.max_rear_speed:
+            self.speed -= 0.5
+        if m == '!':
+            self.breaking()
+        if m == '=':
+            self.rolling()
+        rad = self.rotation * pi / 180
+        self.dx = cos(rad) * self.speed
+        self.dy = sin(rad) * self.speed * -1
+
+    def update(self):
+        self.rect.x += round(self.dx)
+        self.rect.y += round(self.dy)
+
+    def breaking(self):
+        if self.speed >= 1:
+            self.speed -= 1
+        elif self.speed <= -1:
+            self.speed += 1
+        else:
+            self.speed = 0
+
+    def rolling(self):
+        if self.speed > 0:
+            self.speed -= 0.125
+        elif self.speed < 0:
+            self.speed += 0.125
+
+    def turning(self, t):
+        if t == '+' and self.speed:
+            if abs(self.speed) < self.max_front_speed / 2:
+                self.rotation += 0.2 * self.speed
+            else:
+                self.rotation += 0.2 * (self.max_front_speed - self.speed + 5)
+        elif t == '-' and self.speed:
+            if abs(self.speed) < self.max_front_speed / 2:
+                self.rotation -= 0.2 * self.speed
+            else:
+                self.rotation -= 0.2 * (self.max_front_speed - self.speed + 5)
+        self.blitRotate()
+
+    def blitRotate(self):
+        rotated_image = pygame.transform.rotate(self.image, self.rotation)
+        new_rect = rotated_image.get_rect(center=self.image.get_rect(topleft=(self.rect.x, self.rect.y)).center)
+        screen.blit(rotated_image, new_rect.topleft)
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
+    def update_camera(self, gamer):
+        # изменяем ракурс камеры
+        self.update(gamer)
+        # обновляем положение всех спрайтов
+        for sprite in all_sprites:
+            self.apply(sprite)
+
+
+camera = Camera()
+city, player = generate_level(load_level('map.txt'))
+camera.update_camera(player)
+mode = '='
+turning = '!'
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             terminate()
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_w]:
+            mode = '+'
+        elif key[pygame.K_s]:
+            mode = '-'
+        elif key[pygame.K_SPACE]:
+            mode = '!'
+        else:
+            mode = '='
+
+        if key[pygame.K_a]:
+            turning = '+'
+        elif key[pygame.K_d]:
+            turning = '-'
+        else:
+            turning = '!'
+
     screen.fill((0, 0, 0))
     tiles_group.draw(screen)
+    player.turning(turning)
+    player.update_speed(mode)
+    player.update()
+    camera.update_camera(player)
     pygame.display.flip()
     clock.tick(FPS)
